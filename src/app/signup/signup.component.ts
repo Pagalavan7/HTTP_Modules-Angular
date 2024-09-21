278e37e083f5d1778f6f4846b3bfd4d66e86e4d9
Pagalavan7 <pagalavanvetri@gmail.com>
2024-09-21 08:59:33 +0530

    required files generated

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,39 @@
+import { Component, ViewChild } from '@angular/core';
+import { UserService } from '../Services/user.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { User } from '../Models/user.model';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+})
+export class SignupComponent {
+  constructor(private userService: UserService) {}
+  @ViewChild('createUser') createUserForm: NgForm | undefined;
+  ngAfterViewInit() {
+    console.log('in ng after view init method');
+
+    console.log(this.createUserForm);
+  }
+  ngOnChanges() {
+    console.log('in ng on changes method');
+    console.log(this.createUserForm);
+  }
+  onSubmit() {
+    console.log('Form submitted.. values of form is..');
+    console.log(this.createUserForm?.value);
+    const user: User = this.createUserForm?.value;
+    this.userService.createUser(user).subscribe({
+      next: (response) => console.log(response),
+      error: (err) => console.log(err),
+    });
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.createUserForm?.reset();
+  }
+}
