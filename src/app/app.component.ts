a863e8e4a8029cb8a22687d6a39fe583fc959caf
Pagalavan7 <pagalavanvetri@gmail.com>
2024-09-21 08:48:52 +0530

    navbar add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { FormComponent } from './form/form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormComponent],
+  imports: [RouterOutlet, FormComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
